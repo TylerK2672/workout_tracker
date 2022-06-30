ad3c030205d106866d14d0b858b3119cf89866a4
Tyler Kendall <tylerchristiankendall@gmail.com>
2022-06-30 13:04:48 -0600

    first commit

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const SERVER_PORT = process.env.PORT || 3005;
+
+app.get('/', (req, res) =>
+{
+    res.sendFile(path.join(__dirname, '../landing.html'));
+});
+
+app.listen(SERVER_PORT, () =>
+{
+    console.log(`server running on port ${SERVER_PORT}`)
+});
