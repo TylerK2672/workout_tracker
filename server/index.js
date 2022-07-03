3ad35861748aea52958a0a6ed1a7191de229a2ae
Tyler Kendall <tylerchristiankendall@gmail.com>
2022-07-03 15:23:04 -0600

    landing.html fleshed out and addExercise.html added

--- server/index.js
+++ server/index.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '../landing.html'));
 });
 
+app.get('/addExercise', (req, res) =>
+{
+    res.sendFile(path.join(__dirname, '../pages/addExercise.html'));
+});
+
 app.listen(SERVER_PORT, () =>
 {
     console.log(`server running on port ${SERVER_PORT}`)
